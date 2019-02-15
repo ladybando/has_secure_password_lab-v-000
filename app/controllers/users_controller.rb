e1840f810c45456abd363d77f6034e9cecd625fa
Ayana Bando <ayanaflax@yahoo.com>
2019-02-15 20:12:46 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,19 @@ class UsersController < ApplicationController
     return nil unless (salt + hashed) == self.password_digest
   end
   
-  
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to login_path
-    else
-      redirect_to root_path
-  end
+    if params[:user][:password] && params[:user][:password] != params[:user][:password_confirmation]
+     redirect_to new_user_path
+   else
+     @user = User.create(user_params)
+     session[:user_id] = @user.id
+     redirect_to hello_path
+   end
+ end
+
+    private
+
+    def user_params
+     params.require(:user).permit(:name, :password, :password_confirmation)
+   end
 end
