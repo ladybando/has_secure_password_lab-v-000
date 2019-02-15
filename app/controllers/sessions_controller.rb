b3c60c0b30ae35e75c26e101c0d560e139620484
Ayana Bando <ayanaflax@yahoo.com>
2019-02-15 20:06:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to login_path
     else
-      redirect_to new_session_path
+      redirect_to root_path
     end
   end
 end
