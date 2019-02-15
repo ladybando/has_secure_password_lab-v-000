8043310203d39b5c946d866becacfcf17993d70c
Ayana Bando <ayanaflax@yahoo.com>
2019-02-15 20:18:21 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   
   def create
     if params[:user][:password] && params[:user][:password] != params[:user][:password_confirmation]
-     redirect_to login_path
+     redirect_to new_user_path
    else
      @user = User.create(user_params)
      session[:user_id] = @user.id
